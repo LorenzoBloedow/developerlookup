0aca0c3a70b5d6c3b542a87c94040c3049ccefc2
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2022-10-29 18:59:11 -0700

    Create root layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,44 @@
-import { FunctionComponent } from "react";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+"use client"
+
+import React, { FunctionComponent } from "react";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/navbar/Navbar";
+import "../styles/globals.css"
+import { Prompt } from "@next/font/google";
+import { AnalyticsWrapper } from "../components/AnalyticsWrapper";
 
 interface RootLayoutProps {
     children: React.ReactNode;
 }
- 
+
+const prompt = Prompt({
+    subsets: ["latin"],
+    weight: "400",
+});
+
 const RootLayout: FunctionComponent<RootLayoutProps> = ({ children }) => {
 
     return (
-        <html lang="en">
+        <html lang="en" className={prompt.className}>
             <head>
                 <meta name="keywords" content="GitHub, Visualizer, Developer, Lookup, Profile" />
                 <meta name="application-name" content="DeveloperLookup" />
             </head>
-            
+
             <body>
                 <Navbar />
-                { children }
+                <div className="bg-gradient-to-b from-accent-one via-accent-six to-accent-ten w-full h-[140%] absolute top-0 -z-[999999]">
                 <Footer />
+                </div>
+                
+                { children }
+                <AnalyticsWrapper />
             </body>
         </html>
 
     );
 }
- 
+
+
+
 export default RootLayout;
