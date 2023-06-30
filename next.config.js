1cb412f6c77bd421672ccc5918c3c2f0a766ea2b
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2023-06-29 17:11:00 -0700

    Remove experimental section of next.config.js as it's not needed anymore

--- next.config.js
+++ next.config.js
@@ -1,21 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-    allowMiddlewareResponseBody: true
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '/u/*',
-      },
-    ],
-  },
+    reactStrictMode: false,
+    swcMinify: true,
+    images: {
+        remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'avatars.githubusercontent.com',
+            port: '',
+            pathname: '/u/*',
+        },
+        ],
+    }
 }
 
 module.exports = nextConfig;
