30d286ea6092a8b5b6f7354258e287cae8367e7e
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2023-07-09 06:29:39 -0700

    Create a wrapper for skeletons on the profile

---
+++ components/profile/ContentLoaderWrapper.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent, ReactNode } from 'react';
+import ContentLoader from "react-content-loader";
+    
+interface ContentLoaderWrapperProps {
+    children: ReactNode;
+    className?: string;
+    speed?: number;
+    viewBox?: string;
+    color?: string
+}
+ 
+const ContentLoaderWrapper: FunctionComponent<ContentLoaderWrapperProps> = ({ children, className = "", speed = 1, viewBox, color }) => {
+    
+    return (
+        <ContentLoader
+        speed={speed}
+        className={className || "w-full h-full"}
+        backgroundColor="var(--neu-mid)"
+        foregroundColor={color || "#94a3b8"}
+        viewBox={viewBox}
+
+        >
+            {children}
+        </ContentLoader>
+    );
+}
+
+export default ContentLoaderWrapper;
