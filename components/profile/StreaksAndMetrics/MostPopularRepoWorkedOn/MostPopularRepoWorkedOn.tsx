7071299a594d410b49a37b98667841f50b9f6f4a
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2023-07-11 15:56:47 -0700

    Create a component that shows the most popular repo the user worked on

---
+++ components/profile/StreaksAndMetrics/MostPopularRepoWorkedOn/MostPopularRepoWorkedOn.tsx
@@ -0,0 +1,65 @@
+import BypassErrorSerialization from "../../../BypassErrorSerialization";
+import getDateSensitiveData from "../../../../util/getDateSensitiveData";
+import { FaSplotch } from "react-icons/fa";
+import getMostPopularRepo from "../../../../util/getMostPopularRepo";
+import RepoLink from "../../LastCommit/RepoLink";
+    
+interface MostPopularRepoWorkedOnProps {
+    username: string;
+}
+ 
+const MostPopularRepoWorkedOn = async ({ username }: MostPopularRepoWorkedOnProps) => {
+    const result = await getDateSensitiveData(username);
+    const mostPopularRepoResult = await getMostPopularRepo(username); 
+
+    return (
+        <>
+            <BypassErrorSerialization
+            result={result}
+            />
+
+            <BypassErrorSerialization
+            result={mostPopularRepoResult}
+            />
+
+            {
+                (
+                    result.success &&
+                    mostPopularRepoResult.success &&
+                    mostPopularRepoResult.data.name !== result.data.thirtyDays.mostPopularRepoWorkedOn.name    
+                ) &&
+                <div
+                className="flex flex-col items-center gap-1"
+                >
+                    <FaSplotch
+                    className="w-12 h-12 text-[#e4bd11]"
+                    />
+    
+                    <div
+                    className="flex flex-col items-center"
+                    >
+                        <div
+                        className="flex flex-col items-center"
+                        >
+                            <p>
+                                {result.data.thirtyDays.mostPopularRepoWorkedOn.stars}
+                            </p>
+                            <p
+                            className="text-xs"
+                            >
+                                stars
+                            </p>
+                        </div>
+
+                        <RepoLink
+                        repoName={result.data.thirtyDays.mostPopularRepoWorkedOn.name}
+                        repoUrl={result.data.thirtyDays.mostPopularRepoWorkedOn.url}
+                        />
+                    </div>
+                </div>
+            }
+        </>
+    );
+}
+ 
+export default MostPopularRepoWorkedOn;
