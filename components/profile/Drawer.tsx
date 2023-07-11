23f5ece1390c5898480ac5f38c4470683d899527
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2023-07-11 13:20:42 -0700

    Change style of drawer button, change header text, and comment out unused parts

--- components/profile/Drawer.tsx
+++ components/profile/Drawer.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { FunctionComponent, useEffect, useRef, useState } from "react";
 import { CgMenu } from "react-icons/cg";
-import handleEnterKeyPress from "../../util/handleEnterKeyPress";
 
 export type DrawerOptions = "Overview" | "RecentActivity" | "StreaksAndMetrics" | "Milestones" | "SurroundingActivity";
 
@@ -13,7 +12,9 @@ const DrawerOption = ({ changeSelected, children }: { changeSelected: () => void
     return (
         <li
         role="button"
-        className="text-center py-2 text-5xl bg-slate-400 tracking-wide"
+        className="text-center text-xs border-slate-400 rounded-lg
+        active:shadow-inner active:shadow-black bg-slate-300
+        tracking-wide text-slate-400 border-[1px] p-2 font-thin"
         onPointerUp={changeSelected}
         >
             {children}
@@ -40,30 +41,40 @@ const Drawer: FunctionComponent<DrawerProps> = ({ setSelectedValue }) => {
 
     return (
         <menu
-        className="absolute w-3/4 top-0 right-0 flex flex-col z-50"
+        className="absolute w-36 top-4 right-4 flex flex-col z-50 items-end gap-2.5"
         >
             <button
             tabIndex={0}
             aria-owns="profile-drawer"
             aria-expanded={toggle}
-            className="ml-auto right-0 border-2 border-black p-4 bg-slate-400 shadow-lg shadow-black"
+            style={{
+                boxShadow: "9px 9px 13px var(--neu-dark), -9px -9px 30px var(--neu-light)"
+            }}
+            className="p-2 rounded-lg w-12 h-12 bg-neu-mid neu-button"
             onPointerUp={() => setToggle(prev => !prev)}
             >
-                <CgMenu size={75} />
+                <CgMenu
+                className="w-8 h-8"
+                />
             </button>
 
-            <div className="invisible" ref={menuContentRef}>
+            <div
+            className="invisible shadow-black
+            shadow-lg p-5 bg-slate-300 rounded-md
+            flex flex-col gap-5 text-slate-400"
+            ref={menuContentRef}
+            >
                 <header
                 id="drawer-header"
-                className="text-center text-6xl bg-slate-400 tracking-wide font-bold border-2 p-8 rounded-md border-black
-                border-b-slate-300 flex items-center justify-center -mb-2 z-20 shadow-lg shadow-black"
+                className="text-center text-lg bg-slate-300 tracking-wide font-bold
+                flex items-center justify-center z-20 text-slate-400"
                 >
-                        Profile
+                    Profile Section
                 </header>
                 <ul
                 aria-describedby="drawer-header"
                 id="profile-drawer"
-                className="flex flex-col border-black border-x-2 border-b-2 gap-9 bg-slate-400 p-10 rounded-md shadow-lg shadow-black"
+                className="flex flex-col gap-5"
                 >
                     
                     <DrawerOption changeSelected={() => handleSelected("Overview")}>Overview</DrawerOption>
@@ -72,13 +83,13 @@ const Drawer: FunctionComponent<DrawerProps> = ({ setSelectedValue }) => {
                     <DrawerOption changeSelected={() => handleSelected("RecentActivity")}>Recent Activity</DrawerOption>
                     <hr role="separator" />
 
-                    <DrawerOption changeSelected={() => handleSelected("StreaksAndMetrics")}>Streaks and metrics</DrawerOption>
-                    <hr role="separator" />
+                    <DrawerOption changeSelected={() => handleSelected("StreaksAndMetrics")}>Streaks and Metrics</DrawerOption>
+                    {/* <hr role="separator" />
 
                     <DrawerOption changeSelected={() => handleSelected("Milestones")}>Milestones</DrawerOption>
                     <hr role="separator" />
 
-                    <DrawerOption changeSelected={() => handleSelected("SurroundingActivity")}>Surrounding activity</DrawerOption>
+                    <DrawerOption changeSelected={() => handleSelected("SurroundingActivity")}>Surrounding Activity</DrawerOption> */}
                 </ul>
             </div>
         </menu>
