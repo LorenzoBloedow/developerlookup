a7f2b1abb3f8da2b19eff6074c9cce12d55dc1e9
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2023-07-11 15:57:47 -0700

    Create a component to group all streaks and metrics

---
+++ components/profile/StreaksAndMetrics/StreaksAndMetrics.tsx
@@ -0,0 +1,62 @@
+import { Suspense } from "react";
+import MostPopularRepo from "./MostPopularRepo/MostPopularRepo";
+import MostUsedProgrammingLanguages from "./MostUsedProgrammingLanguages/MostUsedProgrammingLanguages";
+import LongestCommitStreak from "./LongestStreak/LongestCommitStreak";
+import LongestBreakStreak from "./LongestStreak/LongestBreakStreak";
+import MostCommitsDay from "./MostCommitsDay/MostCommitsDay";
+import MostActivity from "./MostActivity/MostActivity";
+import CommitAmount from "./LongestStreak/Amount/CommitAmount";
+import PullRequestAmount from "./LongestStreak/Amount/PullRequestAmount";
+
+interface StreaksAndMetrics {
+    username: string
+}
+
+function StreaksAndMetrics({ username }: StreaksAndMetrics) {
+    return (
+        <div className="flex flex-col gap-8">
+            <Suspense>
+                {/** @ts-expect-error */}
+                <MostPopularRepo username={username} />
+            </Suspense>
+
+            <hr className="border-2 border-slate-300" />
+
+            <Suspense>
+                <MostUsedProgrammingLanguages username={username} />
+            </Suspense>
+
+            <Suspense>
+                {/* @ts-expect-error Server Component */}
+                <LongestCommitStreak username={username} />
+            </Suspense>
+
+            <Suspense>
+                {/* @ts-expect-error Server Component */}
+                <LongestBreakStreak username={username} />
+            </Suspense>
+
+            <Suspense>
+                {/* @ts-expect-error Server Component */}
+                <MostCommitsDay username={username} />
+            </Suspense>
+
+            <Suspense>
+                {/* @ts-expect-error Server Component */}
+                <MostActivity username={username} />
+            </Suspense>
+
+            <Suspense>
+                {/* @ts-expect-error Server Component */}
+                <CommitAmount username={username} />
+            </Suspense>
+
+            <Suspense>
+                {/* @ts-expect-error Server Component */}
+                <PullRequestAmount username={username} />
+            </Suspense>
+        </div>
+    );
+}
+
+export default StreaksAndMetrics;
