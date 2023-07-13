8598983447b11a8d45412d01ea6eb04f4b75d7b1
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2023-07-13 06:06:15 -0700

    Change the profile picture spinner's color

--- components/profile/ProfilePicture/ProfilePictureLoading.tsx
+++ components/profile/ProfilePicture/ProfilePictureLoading.tsx
@@ -5,7 +5,8 @@ const ProfilePictureLoading = () => {
 
     return (
         <Spinner
-        className="w-full h-full text-slate-500"
+        className="w-full h-full"
+        color="#94a3b8"
         />
     );
 }
