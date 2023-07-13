20ccef04a28f05047dbfc60aa39a16f7f7751ca6
Lorenzo Bloedow <lorenzobloedow@gmail.com>
2023-07-13 06:49:26 -0700

    Fill all overview components with loading skeletons

--- components/Overview/Overview.tsx
+++ components/Overview/Overview.tsx
@@ -1,10 +1,15 @@
 import { FunctionComponent, Suspense } from "react";
-import Followers from "./Followers";
-import JoinDate from "./JoinDate";
+import Followers from "./Followers/Followers";
+import JoinDate from "./JoinDate/JoinDate";
 import Twitter from "./Twitter/Twitter";
-import Gist from "./Gist";
-import UserType from "./UserType";
+import Gist from "./Gist/Gist";
+import UserType from "./UserType/UserType";
 import Subheading from "../profile/Subheading";
+import FollowersLoading from "./Followers/FollowersLoading";
+import JoinDateLoading from "./JoinDate/JoinDateLoading";
+import TwitterLoading from "./Twitter/TwitterLoading";
+import GistLoading from "./Gist/GistLoading";
+import UserTypeLoading from "./UserType/UserTypeLoading";
 
 interface OverviewProps {
     username: string
@@ -19,32 +24,56 @@ const Overview: FunctionComponent<OverviewProps> = ({ username }) => {
                 Overview
             </Subheading>
             
-            <div className="flex gap-4 md:gap-10">
-                <Suspense>
-                    {/** @ts-expect-error */}
-                    <Followers username={username} />
-                </Suspense>
+            <div className="flex w-full gap-4 justify-center md:justify-between">
+                <div
+                className="w-28 h-36 p-4 flex flex-col
+                text-xs items-center text-center md:text-sm"
+                >
+                    <Suspense fallback={<FollowersLoading />}>
+                        {/** @ts-expect-error */}
+                        <Followers username={username} />
+                    </Suspense>
+                </div>
 
-                <Suspense>
-                    {/** @ts-expect-error */}
-                    <JoinDate username={username} />
-                </Suspense>
+                <div
+                className="w-28 h-36 p-4 flex flex-col text-xs items-center
+                text-center leading-snug md:text-sm"
+                >
+                    <Suspense fallback={<JoinDateLoading />}>
+                        {/** @ts-expect-error */}
+                        <JoinDate username={username} />
+                    </Suspense>
+                </div>
             </div>
    
-            <Suspense>
+            <hr className="border-slate-300 border-b-[1px]" />
+
+            <Suspense fallback={<TwitterLoading />}>
                 {/** @ts-expect-error */}
                 <Twitter username={username} />
             </Suspense>
 
-            <Suspense>
-                {/** @ts-expect-error */}
-                <Gist username={username} />
-            </Suspense>
+            <div
+            className="w-full h-40 py-8 flex flex-col
+            text-xs items-center justify-center
+            gap-1 text-slate-500 md:text-sm"
+            >
+                <Suspense fallback={<GistLoading />}>
+                    {/** @ts-expect-error */}
+                    <Gist username={username} />
+                </Suspense>
+            </div>
 
-            <Suspense>
-                {/** @ts-expect-error */}
-                <UserType username={username} />
-            </Suspense>
+            <hr className="border-slate-300 border-b-[1px]" />
+
+            <div
+            className="w-full h-40 py-8 flex flex-col gap-2 items-center justify-center"
+            >
+                <Suspense fallback={<UserTypeLoading />}>
+                    {/** @ts-expect-error */}
+                    <UserType username={username} />
+                </Suspense>
+            </div>
         </div>
     );
 }
